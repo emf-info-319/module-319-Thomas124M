b34604c6ae8500b3ea95b65a464d0e82dcafd8e7
Thomas124M <thomas.mao@studentfr.ch>
2024-08-29 11:44:28 +0200

    dock

--- exercices/exercice02/Exercice02.java
+++ exercices/exercice02/Exercice02.java
@@ -2,10 +2,13 @@ package exercices.exercice02;
 
 public class Exercice02 {
     public static void main(String[] args) {
-        // début de mon code
-        // système
+        
+        
         System.out.println("Bonjour");
-        System.out.println("ligne 2");
-        // gfged
+       
+      
+
+
+
     }
 }
---
+++ exercices/exercice03/Exercice03.java
@@ -0,0 +1,17 @@
+package exercices.exercice03;
+
+public class Exercice03 { public static void main(String[] args)
+    
+    {int age;//déclarez
+        age = 17;
+   
+String nom = "Thomas";
+System.err.println("je m'appelle "+nom+ " et j'ai "+age );
+
+int age10 = age+10;
+System.err.println("Et dans 10ans j'aurais " + age10 + " ans.");
+
+
+}
+    
+}

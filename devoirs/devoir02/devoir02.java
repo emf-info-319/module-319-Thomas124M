c2c3b82e70beb672b10b561329277fc685fc616d
Thomas124M <thomas.mao@studentfr.ch>
2024-09-19 09:20:57 +0200

    correction

--- devoirs/devoir02/devoir02.java
+++ devoirs/devoir02/devoir02.java
@@ -1,25 +1,28 @@
 package devoirs.devoir02;
 
 public class devoir02 {
+    public final static int CAPACITE_RESERVOIR_A = 3;
+    public final static int CAPACITE_RESERVOIR_B = 5;
+
     public static void main(String[] args) {
         int réservoirA = 0;
         int réservoirB = 0;
-        int remplissage = (int) (Math.random() * (8 - 1 + 1)) + 1;
+        int remplissage = (int) (Math.random() * (CAPACITE_RESERVOIR_A+CAPACITE_RESERVOIR_B - 0 + 1)) + 0;
 
-        System.out.println("il y a 5 litre à remplir.");
+        System.out.println("il y a " +remplissage+ " litre à remplir.");
         do {
             System.out.println("remplisage du réservoir A...");
             remplissage--;
             réservoirA++;
             System.out.println("le réservoir A : " + réservoirA + "le réservoir B : " + réservoirB);
-        } while (réservoirA < 4);
+        } while (réservoirA<=3);
         do {
             System.out.println("remplissage du réservoir B...");
             remplissage--;
             réservoirB++;
             System.out.println("le réservoir A : " + réservoirA + "le réservoir B : " + réservoirB);
 
-        } while (réservoirB < 6);
+        } while (réservoirB <=5);
         if (remplissage == 0) {
             System.out.println("Remplissage terminé!");
         }

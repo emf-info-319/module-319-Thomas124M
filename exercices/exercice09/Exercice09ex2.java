ec1c49cf574a566906ffb2a9dd90964d836891b9
Thomas124M <thomas.mao@studentfr.ch>
2024-09-19 11:45:08 +0200

    exercice

---
+++ exercices/exercice09/Exercice09ex2.java
@@ -0,0 +1,15 @@
+package exercices.exercice09;
+
+public class Exercice09ex2 {
+    public final static int MIN = 1;
+    public final static int MAX = 6;
+    
+
+    public static void main(String[] args) {
+        int [] montableau = new int [10];
+        for (int a = 0; a <montableau.length; a++){
+        int i = ( int ) ( Math.random() * ( MAX - MIN + 1 ) ) + MIN;}
+
+    }
+
+}

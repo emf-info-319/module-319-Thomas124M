4a357c0bc4ccad4fc19084413ee437951e94973c
Thomas124M <thomas.mao@studentfr.ch>
2024-09-24 21:03:26 +0200

    dur

--- devoirs/devoir03/Devoir03.java
+++ devoirs/devoir03/Devoir03.java
@@ -6,21 +6,20 @@ public class Devoir03 {
         int mois = 11;
         int année = 2024;
         int ensemble = 365;
-       
-
         if (jour == 1 && mois == 1) {
             System.out.println("le " + jour + "/" + mois + "/" + année + " est le jour 1 de l'An.");
         }
 
         else if (jour == 31 && mois == 12) {
-            System.out.println("le " + jour + "/" + mois + "/" + année + " est le jour 365 de l'An.");
+            System.out.println("le " + jour + "/" + mois + "/" + année + "est le jour 365 de l'An.");
         }
 
         else {
             System.out.println(
-                    "le " + jour + "/" + mois + "/" + année + " est le jour " + (ensemble - jour ) + " de l'An.");
+                    "le " + jour + "/" + mois + "/" + année + " est le jour " + (ensemble - jour) + " de l'An.");
+
         }
 
-    }
 
+    }
 }
